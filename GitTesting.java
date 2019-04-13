a474632c308a9e790a4b2bc570e8ff4f08bdf2ea
Hua Zeng <zenghua2013@gmail.com>
2019-04-12 23:36:37 -0400

    first commit'

---
+++ GitTesting.java
@@ -0,0 +1,11 @@
+
+public class GitTesting {
+	public static void main(String[] args) {
+		
+		//please change the line in the original version add your name;
+		System.out.println("This is the orignial version:");
+		
+		//feel free to add your changes below
+		System.out.println("hello world");
+	}
+}
