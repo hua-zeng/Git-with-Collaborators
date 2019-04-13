02499ffc18ee780fc79155e8817eefed72e358df
Avatar-Penn <49504936+Avatar-Penn@users.noreply.github.com>
2019-04-13 08:12:33 -0400

    Update GitTesting.java

--- GitTesting.java
+++ GitTesting.java
@@ -3,7 +3,7 @@ public class GitTesting {
 	public static void main(String[] args) {
 		
 		//please change the line in the original version add your name;
-		System.out.println("This is the Avatar's version:");
+		System.out.println("This is Avatar's version:");
 		
 		//feel free to add your changes below
 		System.out.println("hello world - this is my change");
