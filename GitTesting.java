acdf861c73408943f1286c560eba806fbba3a977
Hua Zeng <zenghua2013@gmail.com>
2019-04-13 00:02:27 -0400

    updated file by Hua

--- GitTesting.java
+++ GitTesting.java
@@ -3,9 +3,9 @@ public class GitTesting {
 	public static void main(String[] args) {
 		
 		//please change the line in the original version add your name;
-		System.out.println("This is the orignial version:");
+		System.out.println("This is the Hua's version:");
 		
 		//feel free to add your changes below
-		System.out.println("hello world");
+		System.out.println("hello world - this is my change");
 	}
 }
